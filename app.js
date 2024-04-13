25f73a2eb22acfb88ca87edfd946d6fb97eeed9d
Shriharsh <deshmukhshriharsh7@gmail.com>
2024-04-13 23:55:07 +0530

    Website with backend using HCMERN

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const products_routes = require("./routes/products");
+
+app.get("/",(req, res) => {
+    res.send("Hi, I am live ");
+});
+
+//middleware or to set routers
+app.use("/api/products", products_routes);
+
+
+const start = async () => {
+    try {
+      app.listen(PORT, () => {
+        console.log(`${PORT} Yes I am connected`);
+      });  
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
