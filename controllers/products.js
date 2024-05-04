ca344b4c83a94f7ac374a7489ecaef5723c98eda
Shriharsh <deshmukhshriharsh7@gmail.com>
2024-05-04 14:20:21 +0530

    Update V2.77

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,22 @@
 const Product = require("../models/product");
 
 const getAllProducts = async(req, res) => {
-    const { company } = req.query;
+    const { company, name, featured } = req.query;
     const queryObject = {};
 
     if(company){
         queryObject.company = company;
-        console.log(queryObject.company);
     }
+
+    if(featured){
+        queryObject.featured = featured;
+    }
+
+    if(name){
+        queryObject.name = { $regex : name, $options: "i" };
+    }
+
+    console.log(queryObject.company);
 
     const myData = await Product.find(queryObject);
     res.status(200).json({ myData });
