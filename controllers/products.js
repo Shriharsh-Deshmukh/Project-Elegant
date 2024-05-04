f6d11b0b193915864feb0af771fed50099a13521
Shriharsh <deshmukhshriharsh7@gmail.com>
2024-05-04 14:06:12 +0530

    Update V2.76

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,15 @@
 const Product = require("../models/product");
 
 const getAllProducts = async(req, res) => {
-    const myData = await Product.find(req.query);
+    const { company } = req.query;
+    const queryObject = {};
+
+    if(company){
+        queryObject.company = company;
+        console.log(queryObject.company);
+    }
+
+    const myData = await Product.find(queryObject);
     res.status(200).json({ myData });
 };
 
