747405e5ecf2ec265fd3830ab865f6d78f652e39
Shriharsh <deshmukhshriharsh7@gmail.com>
2024-05-05 21:55:57 +0530

    Update V2.78

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,8 @@
 const Product = require("../models/product");
 
 const getAllProducts = async(req, res) => {
-    const { company, name, featured } = req.query;
+    const { company, name, featured, sort } = req.query;
+    console.log("🚀 ~ file: products.js ~ line 5 ~ getAllProducts ~ sort", sort);
     const queryObject = {};
 
     if(company){
@@ -16,14 +17,21 @@ const getAllProducts = async(req, res) => {
         queryObject.name = { $regex : name, $options: "i" };
     }
 
+    let apiData = Product.find(queryObject);
+
+    if(sort){
+        let sortFix = sort.replace(","," ");
+        apiData = apiData.sort(sortFix);
+    }
+
     console.log(queryObject.company);
 
-    const myData = await Product.find(queryObject);
+    const myData = await apiData;
     res.status(200).json({ myData });
 };
 
 const getAllProductsTesting = async(req, res) => {
-    const myData = await Product.find( req.query );
+    const myData = await Product.find( req.query ).sort("name -price");
     res.status(200).json({ myData });
 };
 
