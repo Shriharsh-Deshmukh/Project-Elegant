70d2f226f84ac9ad0a0e4a6453a9454711224e40
Shriharsh <deshmukhshriharsh7@gmail.com>
2024-05-06 23:14:15 +0530

    Update V3.1 (Pagination)

--- controllers/products.js
+++ controllers/products.js
@@ -31,15 +31,25 @@ const getAllProducts = async(req, res) => {
         apiData = apiData.select(selectFix);
     }
 
-    console.log(queryObject.company);
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+
+    let skip = (page - 1) * limit;
+
+    apiData = apiData.skip(skip).limit(limit);
+
+    console.log(queryObject);
 
     const myData = await apiData;
-    res.status(200).json({ myData });
+    res.status(200).json({ myData, nbHits: myData.length });
 };
 
-const getAllProductsTesting = async(req, res) => {
-    const myData = await Product.find( req.query ).select("name price");
-    res.status(200).json({ myData });
+    const getAllProductsTesting = async(req, res) => {
+    console.log(req.query);
+    const myData = await Product.find( req.query );
+    //sort = name,price;
+
+    res.status(200).json({ myData, nbHits: myData.length });
 };
 
 module.exports = { getAllProducts, getAllProductsTesting };
