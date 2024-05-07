cc2e0d5bfd6482159c6d169f950e483a3c803481
Shriharsh <147980267+Shriharsh-Deshmukh@users.noreply.github.com>
2024-05-07 22:26:13 +0530

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/product");new
 
 const getAllProducts = async(req, res) => {
     const { company, name, featured, sort, select } = req.query;
